a25267d2d83b4363a93ac8321c1de96c463cdb74
zhanglei <408183287@qq.com>
2020-06-15 11:16:16 +0800

    first commit

---
+++ read pcd/ConsoleApplication1.cpp
@@ -0,0 +1,29 @@
+﻿
+#include<iostream>
+#include<pcl/io/pcd_io.h>
+#include<pcl/point_types.h>
+#include<pcl/point_cloud.h>
+#include <pcl/visualization/cloud_viewer.h>
+
+int main(int argc, char *argv[])
+{
+	pcl::PointCloud<pcl::PointXYZRGB>::Ptr  cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+	pcl::io::loadPCDFile<pcl::PointXYZRGB>("F:/vs_c++/pcl_test/point data/1.pcd", *cloud);
+
+	if (pcl::io::loadPCDFile<pcl::PointXYZRGB>("F:/vs_c++/pcl_test/point data/1.pcd", *cloud) == -1)
+	{
+		std::cout << "Cloud reading failed." << std::endl;
+		return (-1);
+	}
+
+	//std::cout << cloud->width << std::endl;
+	//std::cout << cloud->height;
+
+	pcl::visualization::CloudViewer viewer("Simple Cloud Viewer");
+	viewer.showCloud(cloud);
+	while (!viewer.wasStopped())
+	{
+	}
+	system("pause");
+	return (0);
+}
